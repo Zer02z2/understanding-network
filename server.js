03c76c23503a46d605c0126250fe5b907de512b5
Zer02z2 <zongzechan@gmail.com>
2024-09-22 17:36:08 -0400

    Set up

--- server.js
+++ server.js
@@ -1,2 +1,12 @@
 const express = require("express")
+const path = require("path")
 const app = express()
+
+app.listen(3001)
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "index.html"))
+})
+
+const controller = require("./routes/controller")
+app.use("/controller", controller)
