fe4c0e7f3edf0e52ee68ec5643c030dfdba59323
Zer02z2 <zongzechan@gmail.com>
2024-10-27 19:55:23 -0400

    debug

--- server.js
+++ server.js
@@ -1,28 +1,28 @@
 const express = require("express")
 const path = require("path")
-// const cors = require("cors")
+const cors = require("cors")
 const port = 3001
 const rootPath = "undnet"
 const app = express()
 
 app.use(express.json())
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Server is now alive on http://localhost:${port}/${rootPath}/`)
 })
 
-// const io = require("socket.io")(server, {
-//   cors: {
-//     origin: "*",
-//   },
-// })
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+  },
+})
 
 darkRoomApi = require("./routes/darkRoom")
-// const timeMachineApi = require("./routes/timeMachine")(io)
+const timeMachineApi = require("./routes/timeMachine")(io)
 
-//app.use(cors({ origin: "*" }))
+app.use(cors({ origin: "*" }))
 app.use(`/${rootPath}`, express.static(path.join(__dirname, "views")))
 app.use(`/${rootPath}/darkRoom/api`, darkRoomApi)
-// app.use(`/${rootPath}/timeMachine/api`, timeMachineApi)
+app.use(`/${rootPath}/timeMachine/api`, timeMachineApi)
 app.use((req, res) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "index.html"))
+  res.status(404).sendFile(path.join(__dirname, "views/404", "index.html"))
 })
