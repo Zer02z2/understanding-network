860cb9079f8d7a514ac0571fe1690ad3de15b2ca
Zer02z2 <zongzechan@gmail.com>
2024-10-27 18:38:25 -0400

    Socket in router

--- routes/timeMachine.js
+++ routes/timeMachine.js
@@ -1,113 +1,109 @@
 const express = require("express")
-const router = express.Router()
 const { exec } = require("child_process")
-const io = require("socket.io")(3002, {
-  cors: {
-    origin: "*",
-  },
-})
 
-const userLog = {}
-const ipLog = {}
-const bootTime = new Date()
-const serverOffset = bootTime.getTimezoneOffset()
-const hourOffset = -serverOffset / 60
-const timeZone = `UTC ${hourOffset}:00`
+module.exports = (io) => {
+  const router = express.Router()
+  const userLog = {}
+  const ipLog = {}
+  const bootTime = new Date()
+  const serverOffset = bootTime.getTimezoneOffset()
+  const hourOffset = -serverOffset / 60
+  const timeZone = `UTC ${hourOffset}:00`
 
-const init = async () => {
-  const command = 'curl "https://api.ipify.org?format=json"'
-  exec(command, (error, stdout) => {
-    if (error) {
-      console.error(`exec error: ${error}`)
-      return
-    }
-    const serverIp = JSON.parse(stdout).ip
-    initServerInfo(serverIp)
-  })
-}
-const initServerInfo = async (serverIp) => {
-  const serverLocation = await fetchIpLocation(serverIp)
-  ipLog["serverIp"] = {
-    location: serverLocation,
-    publicIp: serverIp,
-    name: "server",
-    timeZone: timeZone,
-    timeDifference: undefined,
-  }
-  userLog["server"] = ipLog["serverIp"]
-}
-
-init()
-
-io.on("connection", (socket) => {
-  const id = socket.id
-  const ip = socket.handshake.address
-
-  socket.on("init", (userData) => {
-    initUser(id, ip, userData)
-  })
-
-  socket.on("userData", (userData) => {
-    if (!userData) return
-    Object.keys(userData).forEach((key) => {
-      if (userData[key]) userLog[id][key] = userData[key]
+  const init = async () => {
+    const command = 'curl "https://api.ipify.org?format=json"'
+    exec(command, (error, stdout) => {
+      if (error) {
+        console.error(`exec error: ${error}`)
+        return
+      }
+      const serverIp = JSON.parse(stdout).ip
+      initServerInfo(serverIp)
     })
-    console.log(userLog)
-    alertUpdates()
-  })
-  socket.on("disconnect", () => {
-    delete userLog[id]
-    alertUpdates()
-  })
-})
-
-const alertUpdates = () => {
-  io.emit("onChange", userLog)
-}
-const initUser = async (id, ip, userData) => {
-  userLog[id] = userData
-  if (ip === "::1") {
-    userLog[id] = { ...userLog[id], ...ipLog.serverIp }
-  } else if (ipLog[ip]) {
-    userLog[id] = { ...userLog[id], ...ipLog[ip] }
-  } else {
-    const location = await fetchIpLocation(ip)
-    ipLog[ip] = {
-      location: location,
-      publicIp: ip,
+  }
+  const initServerInfo = async (serverIp) => {
+    const serverLocation = await fetchIpLocation(serverIp)
+    ipLog["serverIp"] = {
+      location: serverLocation,
+      publicIp: serverIp,
+      name: "server",
+      timeZone: timeZone,
+      timeDifference: undefined,
     }
-    userLog[id] = { ...userLog[id], ...ipLog[ip] }
+    userLog["server"] = ipLog["serverIp"]
   }
-  console.log(userLog)
-  alertUpdates()
-}
 
-const fetchIpLocation = async (ip) => {
-  const url = `http://ip-api.com/json/${ip}`
-  const options = {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-    },
-  }
-  try {
-    const response = await fetch(url, options)
-    const result = await response.json()
-    const location = { city: result.city, country: result.country }
-    return location
-  } catch {
-    const location = { city: undefined, country: undefined }
-    return location
-  }
-}
+  init()
 
-router.get("/serverTime", (req, res) => {
-  const date = new Date()
-  const time = date.getTime()
-  res.status(200).send({
-    time: time,
+  console.log(io)
+
+  io.on("connection", (socket) => {
+    const id = socket.id
+    const ip = socket.handshake.address
+
+    socket.on("init", (userData) => {
+      initUser(id, ip, userData)
+    })
+
+    socket.on("userData", (userData) => {
+      if (!userData) return
+      Object.keys(userData).forEach((key) => {
+        if (userData[key]) userLog[id][key] = userData[key]
+      })
+      alertUpdates()
+    })
+    socket.on("disconnect", () => {
+      delete userLog[id]
+      alertUpdates()
+    })
   })
-})
 
-module.exports = router
+  const alertUpdates = () => {
+    io.emit("onChange", userLog)
+  }
+  const initUser = async (id, ip, userData) => {
+    userLog[id] = userData
+    if (ip === "::1") {
+      userLog[id] = { ...userLog[id], ...ipLog.serverIp }
+    } else if (ipLog[ip]) {
+      userLog[id] = { ...userLog[id], ...ipLog[ip] }
+    } else {
+      const location = await fetchIpLocation(ip)
+      ipLog[ip] = {
+        location: location,
+        publicIp: ip,
+      }
+      userLog[id] = { ...userLog[id], ...ipLog[ip] }
+    }
+    alertUpdates()
+  }
+
+  const fetchIpLocation = async (ip) => {
+    const url = `http://ip-api.com/json/${ip}`
+    const options = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    }
+    try {
+      const response = await fetch(url, options)
+      const result = await response.json()
+      const location = { city: result.city, country: result.country }
+      return location
+    } catch {
+      const location = { city: undefined, country: undefined }
+      return location
+    }
+  }
+
+  router.get("/serverTime", (req, res) => {
+    const date = new Date()
+    const time = date.getTime()
+    res.status(200).send({
+      time: time,
+    })
+  })
+  return router
+}
--- server.js
+++ server.js
@@ -4,8 +4,19 @@ const cors = require("cors")
 const port = 3001
 const rootPath = "undnet"
 const app = express()
+
+const server = app.listen(port, () => {
+  console.log(`Server is now alive on http://localhost:${port}/${rootPath}/`)
+})
+
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+  },
+})
+
 const darkRoomApi = require("./routes/darkRoom")
-const timeMachineApi = require("./routes/timeMachine")
+const timeMachineApi = require("./routes/timeMachine")(io)
 
 app.use(cors({ origin: "*" }))
 app.use(express.json())
@@ -14,8 +25,4 @@ app.use(`/${rootPath}/darkRoom/api`, darkRoomApi)
 app.use(`/${rootPath}/timeMachine/api`, timeMachineApi)
 app.use((req, res) => {
   res.status(404).sendFile(path.join(__dirname, "views/404", "index.html"))
-})
-
-app.listen(port, () => {
-  console.log(`Server is now alive on http://localhost:${port}/${rootPath}/`)
 })
