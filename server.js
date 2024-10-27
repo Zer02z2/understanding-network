550158d9b6edc04b9c0670cca3626c909a73b256
Zer02z2 <zongzechan@gmail.com>
2024-10-27 19:19:17 -0400

    server debug

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = 3001
 const rootPath = "undnet"
 const app = express()
 
-const server = app.listen(port, () => {
+app.listen(port, () => {
   console.log(`Server is now alive on http://localhost:${port}/${rootPath}/`)
 })
 
@@ -15,13 +15,13 @@ const server = app.listen(port, () => {
 //   },
 // })
 
-// const darkRoomApi = require("./routes/darkRoom")
+darkRoomApi = require("./routes/darkRoom")
 // const timeMachineApi = require("./routes/timeMachine")(io)
 
 //app.use(cors({ origin: "*" }))
 app.use(express.json())
 app.use(`/${rootPath}`, express.static(path.join(__dirname, "views")))
-// app.use(`/${rootPath}/darkRoom/api`, darkRoomApi)
+app.use(`/${rootPath}/darkRoom/api`, darkRoomApi)
 // app.use(`/${rootPath}/timeMachine/api`, timeMachineApi)
 app.use((req, res) => {
   res.status(404).sendFile(path.join(__dirname, "views/404", "index.html"))
