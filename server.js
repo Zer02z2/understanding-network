65b3784729d59ea154478d1cccccc9eef030aa99
Zer02z2 <zongzechan@gmail.com>
2024-10-28 00:40:06 -0400

    Build

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path")
 const port = 3001
 const rootPath = "undnet"
 const app = express()
-const cors = require("cors")
+//const cors = require("cors")
 
 app.use(express.json())
 app.set("trust proxy", true)
@@ -11,16 +11,12 @@ app.set("trust proxy", true)
 const server = app.listen(port, () => {
   console.log(`Server is now alive on http://localhost:${port}/${rootPath}/`)
 })
-const io = require("socket.io")(server, {
-  cors: {
-    origin: "*",
-  },
-})
+const io = require("socket.io")(server)
 
 const darkRoomApi = require("./routes/darkRoom")
 const timeMachineApi = require("./routes/timeMachine")(io)
 
-app.use(cors({ origin: "*" }))
+//app.use(cors({ origin: "*" }))
 app.use(`/${rootPath}`, express.static(path.join(__dirname, "views")))
 app.use(`/${rootPath}/darkRoom/api`, darkRoomApi)
 app.use(`/${rootPath}/timeMachine/api`, timeMachineApi)
