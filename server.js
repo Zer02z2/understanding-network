d7f6cb4d32783f5a7dd47f3075c5d9ee4af1f4ee
Zer02z2 <zongzechan@gmail.com>
2024-10-27 20:16:35 -0400

    debug

--- server.js
+++ server.js
@@ -3,26 +3,26 @@ const path = require("path")
 const port = 3001
 const rootPath = "undnet"
 const app = express()
-const cors = require("cors")
+//const cors = require("cors")
 
 app.use(express.json())
 const server = app.listen(port, () => {
   console.log(`Server is now alive on http://localhost:${port}/${rootPath}/`)
 })
 
-const io = require("socket.io")(server, {
-  cors: {
-    origin: "*",
-  },
-})
+// const io = require("socket.io")(server, {
+//   cors: {
+//     origin: "*",
+//   },
+// })
 
 const darkRoomApi = require("./routes/darkRoom")
-const timeMachineApi = require("./routes/timeMachine")(io)
+//const timeMachineApi = require("./routes/timeMachine")(io)
 
-app.use(cors({ origin: "*" }))
+//app.use(cors({ origin: "*" }))
 app.use(`/${rootPath}`, express.static(path.join(__dirname, "views")))
 app.use(`/${rootPath}/darkRoom/api`, darkRoomApi)
-app.use(`/${rootPath}/timeMachine/api`, timeMachineApi)
+//app.use(`/${rootPath}/timeMachine/api`, timeMachineApi)
 app.use((req, res) => {
   //res.status(404).sendFile(path.join(__dirname, "views/404", "index.html"))
   res.status(404).send()
