80df861dd159dc31d55e21c0e8d00342716a1681
Zer02z2 <zongzechan@gmail.com>
2025-01-06 02:33:11 -0500

    peer update

--- peerServer.js
+++ peerServer.js
@@ -1,15 +1,23 @@
-import express from "express"
-import { ExpressPeerServer } from "peer"
+// import express from "express"
+// import { ExpressPeerServer } from "peer"
 
-const port = 9000
-const rootPath = "peerjs"
+// const port = 9000
+// const rootPath = "peerjs"
 
-const app = express()
-const server = app.listen(port, () => {
-  console.log(`Server is now alive on http://localhost:${port}/${rootPath}/`)
-})
-const peerServer = ExpressPeerServer(server, {
+// const app = express()
+// const server = app.listen(port, () => {
+//   console.log(`Server is now alive on http://localhost:${port}/${rootPath}/`)
+// })
+// const peerServer = ExpressPeerServer(server, {
+//   proxied: true,
+//   debug: true,
+// })
+// app.use(`/${rootPath}`, peerServer)
+
+import { PeerServer } from "peer"
+
+const peerServer = PeerServer({
+  port: 9000,
   proxied: true,
   debug: true,
 })
-app.use(`/${rootPath}`, peerServer)
