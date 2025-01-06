a04b18c2f378802953e4c6d4c517006bac6e989c
Zer02z2 <zongzechan@gmail.com>
2025-01-06 02:25:47 -0500

    debug

--- peerServer.js
+++ peerServer.js
@@ -3,5 +3,7 @@ import { ExpressPeerServer } from "peer"
 
 const app = express()
 const server = app.listen(9000)
-const peerServer = ExpressPeerServer(server)
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+})
 app.use("/peerjs", peerServer)
