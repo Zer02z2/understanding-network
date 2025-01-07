075b8c613833dc9c625b82a1a537d8e9bf5a9f9e
Zer02z2 <zongzechan@gmail.com>
2025-01-06 19:36:12 -0500

    Handle close call

--- routes/screenShare.js
+++ routes/screenShare.js
@@ -4,10 +4,18 @@ export default (io) => {
   const router = express.Router()
 
   io.of("/screenShare").on("connection", (socket) => {
+    const userData = { id: undefined, room: undefined }
     socket.on("join-room", (roomId, userId) => {
+      userData.id = userId
+      userData.room = roomId
       console.log(roomId, userId)
       socket.join(roomId)
       socket.to(roomId).emit("user-connected", userId)
+    })
+    socket.on("disconnect", () => {
+      console.log("user disconnected: ", userData.id)
+      const { id, room } = userData
+      socket.to(room).emit("close-call", id)
     })
   })
   return router
